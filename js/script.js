4e98f075df724447d8eb0c2daf62bc63e6143020
Ed <example@gmail.com>
2021-09-22 22:26:11 +0300

    first commit comment

---
+++ js/script.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Pipa';
+
+alert(`Привет, ${user}`);
+
+// alert('Hello');
+
+// const result = confirm('Алё йоба?');
+// console.log(result);
+
+// const answer = +prompt('Ты кто, мальчик?', 'Хрен в пальто');
+// // console.log(typeof(answer));
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Имя скажи?', '');
+// answers[1] = prompt('Фамилия?', '');
+// answers[2] = prompt('Сколько лет йопта щегол?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const person = 'Alex';
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpeg', 6, 'apple.bmp', {}, []];
+// console.log(arr[0]);
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
+
+// alert(5);
+// [].push('a');
