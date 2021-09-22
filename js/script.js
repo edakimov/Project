d124d289c505a1216fb6cfca15123346d4a7fdaf
Ed <example@gmail.com>
2021-09-22 22:56:22 +0300

    v.3 add

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 "use strict";
 
-const category = 'toys';
 
-console.log(`https://someurl.com/${category}/5`);
 
-const user = 'Pipa';
+// console.log('arr' + ' - object');
 
-alert(`Привет, ${user}`);
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+// const isChecked = false,
+//       isClose = false;
+
+// // console.log(isChecked && isClose);
+// // console.log(isChecked || isClose);
+// console.log(isChecked || !isClose);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Pipa';
+
+// alert(`Привет, ${user}`);
 
 // alert('Hello');
 
